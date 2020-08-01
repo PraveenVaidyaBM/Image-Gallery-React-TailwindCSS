fc7b9e52a4837109803628ea95fdc23d19e7837b
Praveen B M <49068007+PraveenVBM@users.noreply.github.com>
2020-08-01 18:24:49 +0530

    react image gallery

---
+++ src/components/ImageSearch.js
@@ -0,0 +1,29 @@
+import React,  {useState} from 'react';
+
+const ImageSearch = ({searchText}) => {
+    const [text, setText] = useState('');
+
+    const onSubmit = (e)=>{
+        e.preventDefault();
+        searchText(text);
+    }
+
+    return (
+        <div>
+            <div className="max-w-sm rounded overflow-hidden my-10 mx-auto">
+                <form onSubmit={onSubmit} className="w-full max-w-sm">
+                    <div className="flex items-center border-b border-b-2 border-teal-500 py-2"></div>
+                    <input onChange={e=>setText(e.target.value)} className="appearence-none bg-transparent border-none w-full text-gray-700
+                    mr-3 py-1 px-2 loading-tight focus:outline-none" type="text" 
+                    placeholder="search images"></input>
+                    <button className="flex-shrink-0 bg-teal-500 hover:by-teal-700 border-total-500
+                    hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="submit" align="center">
+                        Search
+                    </button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default ImageSearch;
